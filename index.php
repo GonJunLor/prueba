80cf1ea052d08df044ce853df43a7e4fe7942ebc
Gonzalo Junquera Lorenzo <gonzalo.junlor@educa.jcyl.es>
2025-11-08 20:32:39 +0100

    Inicio

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<h1>CALCULADORA</h1>';
+        $x = 3;
+        $y = 5;
+
+        $x*=5;
+        $oFechaPrueba = new DateTime();
+        $suma = $x + $y;
+
+        echo $suma;
+    ?>
+</body>
+</html>
